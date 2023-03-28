847e1116ac4fcdf85c6722da6459e4394cd37b85
Elman <elmanehmedov66@gmail.com>
2023-03-28 10:09:15 +0400

    recoil init

---
+++ src/TextInput.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {useRecoilState, atom} from 'recoil'
+const textState = atom({
+    key: 'textState', // unique ID (with respect to other atoms/selectors)
+    default: '', // default value (aka initial value)
+  });
+
+const TextInput = () => {
+     
+       const [text, setText]=useRecoilState(textState);
+       const onChange = (event) => {
+        setText(event.target.value);
+      };
+  return (
+    <div>
+        <input type="text" value={text} onChange={onChange} />
+      <br />
+      Echo: {text}
+    </div>
+  )
+}
+
+export default TextInput
